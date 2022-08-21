2e2f9fb934e505e7c56b5695015a1cfb327bc188
banhyungil <byban4186@gmail.com>
2022-08-21 13:23:24 +0900

    로그인 페이지 연결 완료

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app.register_blueprint(movie_cinema_bp)
 
 @app.route('/')
 def home():
-    return render_template('/fandiary/fandiary.html')
+    return render_template('/login.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000",debug=True)
